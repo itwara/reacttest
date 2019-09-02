0e0a6733268ccdf060bc856f0e3d394c3867cf02
itwara <itwara@sina.com>
2019-09-02 17:52:52 +0800

    适用新的hook

--- src/views/lifeCircle/children/index.js
+++ src/views/lifeCircle/children/index.js
@@ -3,10 +3,15 @@ import React, { Component } from 'react';
 class Chirdren extends Component {
   constructor() {
     super()
+    this.state = {}
     console.log('Chirdren constructor')
   }
-  componentWillMount() {
-    console.log('Chirdren componentWillMount')
+  // componentWillMount() {
+  //   console.log('Chirdren componentWillMount')
+  // }
+  static getDerivedStateFromProps(props, state) {
+    console.log('Chirdren getDerivedStateFromProps')
+    return Object.assign({}, state)
   }
   componentDidMount() {
     console.log('Chirdren componentDidMount')
@@ -15,11 +20,15 @@ class Chirdren extends Component {
     console.log('Chirdren shouldComponentUpdate')
     return true
   }
-  componentWillReceiveProps() {
-    console.log('Chirdren componentWillReceiveProps')
-  }
-  componentWillUpdate() {
-    console.log('Chirdren componentWillUpdate')
+  // componentWillReceiveProps() {
+  //   console.log('Chirdren componentWillReceiveProps')
+  // }
+  // componentWillUpdate() {
+  //   console.log('Chirdren componentWillUpdate')
+  // }
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('Chirdren getSnapshotBeforeUpdate')
+    return Object.assign({}, prevState)
   }
   componentDidUpdate(){
     console.log('Chirdren componentDidUpdate')
